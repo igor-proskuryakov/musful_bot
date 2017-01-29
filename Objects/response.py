f83105e7b5d848b217e48737e6a497dcd5235867
Igor <igorproskur94@gmail.com>
2017-01-29 10:14:21 +0300

    fix CommandResponse

--- Objects/response.py
+++ Objects/response.py
@@ -5,35 +5,32 @@ from Commands.commands_list import *
 
 
 def sendResponse(response_url, method, json_data):
-        request = requests.post(response_url + method, json=json_data)
-        return True
-
+    request = requests.post(response_url + method, json=json_data)
+    return True
 
 
 class BadRequest():
     def send(self, response_url):
         pass
 
-
     def __init__(self, update):
         self.method = 'sendmessage'
         pass
+
 
 class MessageResponse():
     def send(self, response_url):
         sendResponse(response_url, self.method, self.json_data)
 
-
     def responseToMessage(self):
         self.method = 'sendmessage'
-        self.message_text = u'Hello, '+self.response_to.fullname+'!' + \
-            u'\nSend me /start command for more information.'
+        self.message_text = u'Hello, ' + self.response_to.fullname + '!' + \
+                            u'\nSend me /start command for more information.'
         self.json_data = {
             'chat_id': self.response_to.id,
             'text': self.message_text
         }
         return True
-
 
     def __init__(self, update):
         self.response_to = update.user
@@ -41,8 +38,8 @@ class MessageResponse():
         self.update = update
         self.responseToMessage()
 
-class CommandResponse:
 
+class CommandResponse:
     def send(self, response_url):
         sendResponse(response_url, self.method, self.json_data)
 
@@ -53,15 +50,11 @@ class CommandResponse:
 
         return True
 
-    def __init__(self, response_url, update):
+    def __init__(self, update):
         self.type = 'CommandResponse'
         self.response_to = update.user
         self.update = update
         self.responseToCommand()
-
-
-
-
 
 
 def newResponse(update):
@@ -72,4 +65,4 @@ def newResponse(update):
     if not getattr(update, 'update_type', None) or getattr(update, 'update_type', None) not in responses:
         return BadRequest(update)
     else:
-        return  responses[update.update_type](update)
+        return responses[update.update_type](update)
